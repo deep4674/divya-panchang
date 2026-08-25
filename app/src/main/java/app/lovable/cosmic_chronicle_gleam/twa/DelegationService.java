32500ffb7e6bfdeaf6a37f6379236eee4be99099
deep4674 <da4674@gmail.com>
2026-08-25 14:16:27 +0530

    Add files via upload

---
+++ app/src/main/java/app/lovable/cosmic_chronicle_gleam/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.lovable.cosmic_chronicle_gleam.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
